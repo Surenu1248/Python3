0fee64ef99abc2dfef7dbdd30c98203a9538e456
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2019-09-27 10:43:29 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,10 @@
+# Write a program to find the biggest of 3 numbers (Use If Condition)
+
+
+a = 10; b = 20; c = 30
+if a > b:
+    print(a, " is Biggest number.....")
+elif b > c:
+    print(b, " is Biggest number.....")
+else:
+    print(c, " is Biggest number.....")
