9fbbe4f651925a3e0fa81d3e38e8d68d26293446
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2019-09-27 12:21:41 +0530

    Create eighth.py

---
+++ eighth.py
@@ -0,0 +1,17 @@
+# Repeat program 7 with Tuples (Take example from Tutorial)
+
+tpl1 = (10,20,30,40,50,60,70,80,90,100,110)
+tpl2 = (11,22,33,44,55,66,77,88,99)
+
+
+# Printing all Elements.....
+print("List Elements are: ", tpl1)
+
+# Slicing Operations.....
+print("Slicing Operation: ", tpl1[3:6])
+
+# Repetition.....
+print("Repetition of list for 5 times: ", (tpl1,) * 5)
+
+# Concatenation of lst and lst2 
+print("Combination of lst and lst2: ", tpl1 + tpl2)
