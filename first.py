e073fa4d599cf523e522ceae63b1192b46dcb1b0
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2019-09-27 12:36:13 +0530

    Update first.py

--- first.py
+++ first.py
@@ -2,43 +2,43 @@
 
 def add(a,b):
 
-    return a+b
+    return a + b
 
 
 
 
 def sub(a,b):
 
-    return a-b
+    return a - b
 
 
 
 
 def mul(a,b):
 
-    return a*b
+    return a * b
 
 
 
 
 def div1(a,b):
 
-    return a/b
+    return a / b
 
 
 
 
 def div2(a,b):
 
-    return a//b
+    return a // b
 
 
-print(add(10,5))
+print(add(10, 5))
 
-print(sub(10,5))
+print(sub(10, 5))
 
-print(mul(10,5))
+print(mul(10, 5))
 
-print("Floating Division: ",div1(10,5))
+print("Floating Division: ", div1(10, 5))
 
-print("Integer Division: ",div2(10,5))
+print("Integer Division: ", div2(10, 5))
