41dc2f226abd1e2d8a845dfebeb4ac0b9e789c50
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2019-09-27 10:47:50 +0530

    Create third.py

---
+++ third.py
@@ -0,0 +1,11 @@
+# Write a program to find given number is odd or Even
+
+def even_or_odd(a):
+    if a % 2 == 0:
+        print(a, " is Even Number.....")
+    else: 
+        print(a, " is Odd Number.....")
+
+
+print(even_or_odd(10))
+print(even_or_odd(5))
