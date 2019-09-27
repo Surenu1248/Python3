f69a637ebe06e96c15c4ba5f1b23f945d615749a
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2019-09-27 12:36:40 +0530

    Create tenth.py

---
+++ tenth.py
@@ -0,0 +1,56 @@
+# Using assignment operators, perform following operations
+# Addition, Substation, Multiplication, Division, Modulus, Exponent and Floor division operations
+
+def add(a,b):
+
+    return a + b
+
+
+
+
+def sub(a,b):
+
+    return a - b
+
+
+
+
+def mul(a,b):
+
+    return a * b
+
+
+
+
+def div1(a,b):
+
+    return a / b
+
+
+
+
+def div2(a,b):
+
+    return a // b
+
+def modulus(a,b):
+    return a % b
+    
+def expo(a,b):
+    return a ** b
+
+print("Addition: ", add(10, 5))
+
+print("Substraction: ", sub(10, 5))
+
+print("Multiplication: ", mul(10, 5))
+
+print("Floating Division: ", div1(10, 5))
+
+print("Floor Division: ", div2(10, 5))
+
+print("Modulus : ", modulus(10,4))
+
+print("Exponential: ", expo(2,3))
+
+
