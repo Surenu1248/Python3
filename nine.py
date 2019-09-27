0f1b21cf4a935740bd19acc967487b7c9a5b49ad
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2019-09-27 12:30:15 +0530

    Create nine.py

---
+++ nine.py
@@ -0,0 +1,37 @@
+# Write program to Add, Subtract, Multiply, Divide 2 Complex numbers.
+
+
+def add(a,b):
+
+    return a + b
+
+
+
+
+def sub(a,b):
+
+    return a - b
+
+
+
+
+def mul(a,b):
+
+    return a * b
+
+
+
+
+def div1(a,b):
+
+    return a / b
+
+
+
+print("Addition:", add(10 + 7j, 5 + 3j))
+
+print("Substraction: ", sub(10 + 7j, 5 + 3j))
+
+print("Multiplication: ", mul(10 + 7j, 5 + 3j))
+
+print("Division: ", div1(10 + 7j, 5 + 3j))
